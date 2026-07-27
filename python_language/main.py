34b21edf25ffae653744aaa0d1cc2d813fba8db2
leeseongjin <doyeonlove09@gmail.com>
2026-07-27 11:47:18 +0900

    calculator import

---
+++ python_language/main.py
@@ -0,0 +1,12 @@
+# import calculator as cl # .py는 적지 않음
+# from calculator import *
+from calculator import add,sub
+
+num1 = 100
+num2 = 6
+
+# print("더히가:", cl.add(num1,num2))
+# print("빼기:", cl.sub(num1,num2))
+
+print("더하기:", add(num1,num2))
+print("빼기:", sub(num1,num2))
